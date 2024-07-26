5afbefa9e838b157f06463624ec6926a989161c8
godwinn22 <godwinohazulike@gmail.com>
2024-07-26 22:26:24 +0100

    6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((res) => {
-    res.map((stats) => ({
-      status: stats.status,
-      value: stats.status === 'fulfilled' ? stats.value : String(stats.reason),
-    }));
-  });
+  ]).then((res) => res.map((stats) => ({
+    status: stats.status,
+    value:
+                stats.status === 'fulfilled'
+                  ? stats.value
+                  : String(stats.reason),
+  })));
 }
