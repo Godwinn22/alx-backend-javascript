909f74c64e5d5346e7a9e17238f11eab8c6b4960
godwinn22 <godwinohazulike@gmail.com>
2024-09-23 00:53:57 +0100

    1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 // Display the initial message
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 // Listen for data input (user's name)
 process.stdin.on('data', (data) => {
   // Trim to remove extra new lines/spaces
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+//   const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${data}`);
 
   // End the input process (optional for interactive mode)
   process.stdin.end();
@@ -13,5 +13,5 @@ process.stdin.on('data', (data) => {
 
 // Handle the end of the program (when the input ends)
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
